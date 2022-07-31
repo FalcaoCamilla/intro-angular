db3c63f5d8a67830947128fd1cab92391194520f
camilla.falcao <millafalcao.acad@gmail.com>
2022-07-31 12:47:54 -0300

    ngDoCheck

--- primeiroProjeto/src/app/app.component.ts
+++ primeiroProjeto/src/app/app.component.ts
@@ -1,19 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: `
-  <app-title title="Olá, mundo"></app-title>
+  {{valor}}
+  <button (click)="adicionar()">Adicionar</button>
   <router-outlet></router-outlet>`
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewChecked, AfterViewInit {
+
+  public valor: number = 1;
+
   constructor(){
 
   }
 
-  ngOnInit(): void{
-    
+  public adicionar(): number{
+    return this.valor +=1;
   }
-  
+
+  ngOnInit(): void{
+
+  }
+
+  ngDoCheck(): void {
+    console.log('ngDoCheck - Houve alteração')
+  }
+
+  ngAfterContentInit(): void {
+
+  }
+
+
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked - Houve alteração neste content')
+  }
+
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit - Houve alteração após essa view')
+  }
+
+  ngAfterViewChecked(): void {
+
+  }
+
 }
