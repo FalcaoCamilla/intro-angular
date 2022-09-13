0ed7a9c17a36a5875175b19bb3382a4324df16ce
FalcaoCamilla <millafalcao.acad@gmail.com>
2022-09-13 09:47:26 -0300

    services

---
+++ meu-primeiro-app/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <app-food-list></app-food-list>
+  `,
+})
+export class AppComponent {
+  title = 'meu-primeiro-app';
+}
